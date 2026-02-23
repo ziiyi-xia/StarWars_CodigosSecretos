01a751983332078c75eb41412065b6266a209759
ziiyi-xia <alegriax027@gmail.com>
2026-02-23 01:33:19 +0100

    Create perder.java

---
+++ perder.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class perder{
+    public static void main(String[] args)throws IOException, InterruptedException{
+        limpiar.clear();
+        System.out.println("Ese no era el código correcto…\n" + " La misión ha sido un fracaso…\n" + 
+        " :( :( :( Todavía no eres un Maestro Jedi de las Matemáticas.\n" + 
+        "¡Vuelve a intentarlo!");
+
+        fin.main(new String[]{});
+    }
+}
