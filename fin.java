c3f55ac39731dc0c4c300721985b9cda54186f2f
ziiyi-xia <alegriax027@gmail.com>
2026-02-23 01:32:23 +0100

    Create fin.java

---
+++ fin.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class fin{
+    public static void main(String[] args)throws IOException, InterruptedException{
+        limpiar.clear();
+        System.out.println("Gracias por jugar :D");
+    }
+}
