0e582d8cd0a4d330ccab71e6c89750b0ad1116d8
ziiyi-xia <alegriax027@gmail.com>
2026-02-23 01:32:48 +0100

    Create ganar.java

---
+++ ganar.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class ganar {
+    public static void main(String[] args)throws IOException, InterruptedException{
+        limpiar.clear();
+
+        System.out.println("Luke Skywalker introduce el tiempo correcto, activa el temporizador y empiezan a sonar las alarmas.\n" + 
+"Salen de allí corriendo, no hay tiempo que perder.\n" + 
+"La nave se convierte en un hervidero de soldados de arriba a abajo y entre el caos que les rodea consiguen llegar a la nave y salir de allí a toda prisa.\n" + 
+"A medida que se alejan observan por la ventana la imagen de la colosal estrella de la muerte explotando en el silencio del espacio, desapareciendo para siempre junto a los restos del malvado imperio.\n" + 
+"¡Has salvado la galaxia gracias a la Fuerza Jedi de las matemáticas! Enhorabuena ;D");
+        Thread.sleep(5000);
+        fin.main(new String[]{});
+    }
+}
